6c80a8fbabc102666aad507f5d2949fe26abc401
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2021-03-27 17:15:47 +0100

    abc

--- IS_Bolnica/IS_Bolnica/MainWindow.xaml.cs
+++ IS_Bolnica/IS_Bolnica/MainWindow.xaml.cs
@@ -35,5 +35,13 @@ namespace IS_Bolnica
             this.Close();
 
         }
+
+        private void ButtonSekretarClicked(object sender, RoutedEventArgs e)
+        {
+            SekretarWindow sw = new SekretarWindow();
+            sw.Show();
+            this.Close();
+
+        }
     }
 }
