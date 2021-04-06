36d9f3d739e010039c9a1704e8841e8f737b4926
Sara Poparic <poparic.ra60.2018@uns.ac.rs>
2021-04-06 22:30:18 +0200

    premestanje metoda

--- IS_Bolnica/IS_Bolnica/EditWindow.xaml.cs
+++ IS_Bolnica/IS_Bolnica/EditWindow.xaml.cs
@@ -2,65 +2,59 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace IS_Bolnica
 {
     public partial class EditWindow : Window
     {
         private int selectedRoom;
+        private RoomRecord editRoom = new RoomRecord();
+        private RoomRecord oldRoom = new RoomRecord();
+        private Director director = new Director();
 
         public EditWindow(int room)
         {
             InitializeComponent();
 
-                //all rooms
-                RoomRecordFileStorage storage = new RoomRecordFileStorage();
-                List<RoomRecord> rooms = storage.loadFromFile("Sobe.json");
+           /* roomBox.Text = room.Id;
+            wardBox.Text = room.HospitalWard;
+            purposeBox.Text = room.roomPurpose.Name;
 
-                roomBox.Text = rooms.ElementAt(room).Id;
-                wardBox.Text = rooms.ElementAt(room).HospitalWard;
+            oldRoom = room;
 
-                RoomPurpose purpose = rooms.ElementAt(room).roomPurpose;
-                purposeBox.Text = purpose.Name;
+            selectedRoom = index;
 
-                selectedRoom = room;
+            UpravnikWindow uw = new UpravnikWindow(director);
+            uw.Close();*/
 
-                Director director = new Director();
-                UpravnikWindow uw = new UpravnikWindow(director);
-                uw.Close();
-            
+            //all rooms
+             RoomRecordFileStorage storage = new RoomRecordFileStorage();
+             List<RoomRecord> rooms = storage.loadFromFile("Sobe.json");
+
+             roomBox.Text = rooms.ElementAt(room).Id;
+             wardBox.Text = rooms.ElementAt(room).HospitalWard;
+
+             RoomPurpose purpose = rooms.ElementAt(room).roomPurpose;
+             purposeBox.Text = purpose.Name;
+
+             selectedRoom = room;
+
+             UpravnikWindow uw = new UpravnikWindow(director);
+             uw.Close(); 
         }
 
         private void DoneButton(object sender, RoutedEventArgs e)
         {
-            RoomRecord newRoom = new RoomRecord();
-            newRoom.Id = roomBox.Text;
-            newRoom.HospitalWard = wardBox.Text;
-
+            editRoom.Id = roomBox.Text;
+            editRoom.HospitalWard = wardBox.Text;
             RoomPurpose purpose = new RoomPurpose { Name = purposeBox.Text };
-            newRoom.roomPurpose = purpose;
+            editRoom.roomPurpose = purpose;
 
             RoomRecordFileStorage storage = new RoomRecordFileStorage();
-            List<RoomRecord> rooms = storage.loadFromFile("Sobe.json");
-            rooms.ElementAt(selectedRoom).Id = newRoom.Id;
-            rooms.ElementAt(selectedRoom).HospitalWard = newRoom.HospitalWard;
-            rooms.ElementAt(selectedRoom).roomPurpose = newRoom.roomPurpose;
-            storage.saveToFile(rooms, "Sobe.json");
-
-            Director d = new Director();
-            UpravnikWindow uw = new UpravnikWindow(d);
-            uw.lvDataBinding.Items.Refresh();
-            uw.Show();
+            storage.EditRoom(selectedRoom, editRoom);
+            
             this.Close();
         }
 
@@ -72,11 +66,13 @@ namespace IS_Bolnica
 
         private void CancelButton(object sender, RoutedEventArgs e)
         {
-            Director d = new Director();
-            UpravnikWindow uw = new UpravnikWindow(d);
-            uw.lvDataBinding.Items.Refresh();
-            uw.Show();
             this.Close();
+        }
+
+        private void ClosingWindow(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            UpravnikWindow uw = new UpravnikWindow(director);
+            uw.Show();
         }
     }
 }
--- IS_Bolnica/IS_Bolnica/Model/RoomRecordFileStorage.cs
+++ IS_Bolnica/IS_Bolnica/Model/RoomRecordFileStorage.cs
@@ -21,9 +21,56 @@ namespace Model
             return rooms;
         }
 
-        public void Save(RoomRecord newRoom)
+        public void AddRoom(RoomRecord newRoom)
         {
+            rooms = loadFromFile("Sobe.json");
             rooms.Add(newRoom);
+            saveToFile(rooms, "Sobe.json");
+        }
+
+        public void DeleteRoom(int room)
+        {
+            if (room < 0)
+                return;
+
+            rooms = loadFromFile("Sobe.json");
+            rooms.RemoveAt(room);
+            saveToFile(rooms, "Sobe.json");
+        }
+
+        public void EditRoom(int selectedRoom, RoomRecord newRoom)
+        {
+            rooms = loadFromFile("Sobe.json");
+            rooms.RemoveAt(selectedRoom);
+            rooms.Insert(selectedRoom, newRoom);
+            saveToFile(rooms, "Sobe.json");
+
+            //int index = rooms.IndexOf(oldRoom);
+
+
+            /*for (int j = 0; j < rooms.Count; j++)
+            {
+                if (j == index)
+                {
+                    rooms.RemoveAt(j);
+                    rooms.Insert(j, newRoom);
+
+                    return;
+                }
+            }*/
+
+           /* int index;
+            foreach(RoomRecord room in rooms)
+            {
+                if(room.Id == oldRoom.Id)
+                {
+                    index = rooms.IndexOf(room);
+                    rooms.RemoveAt(index);
+                    rooms.Insert(index, newRoom);
+                }
+            }*/
+
+            //saveToFile(rooms, "Sobe.json");
         }
 
         public void saveToFile(List<RoomRecord> rooms, string fileName)
