6d37a9c8f3ee0318db9e4e7fe4de7ba7c00687b4
Marija Petrovic <73654047+marija5rovic@users.noreply.github.com>
2021-05-31 17:06:40 +0200

    details

--- IS_Bolnica/IS_Bolnica/DoctorUI/AnamnesisWindow.xaml.cs
+++ IS_Bolnica/IS_Bolnica/DoctorUI/AnamnesisWindow.xaml.cs
@@ -12,14 +12,66 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using IS_Bolnica.Model;
+using IS_Bolnica.Services;
 
 namespace IS_Bolnica.DoctorUI
 {
     public partial class AnamnesisWindow : Window
     {
+        private UserService userService = new UserService();
         public AnamnesisWindow(Appointment examination, List<Appointment> loggedAppointments)
         {
             InitializeComponent();
+        }
+
+        private void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult messageBox = MessageBox.Show("Da li ste sigurni da želite da izađete?",
+                "Izmena operacije", MessageBoxButton.YesNo);
+
+            switch (messageBox)
+            {
+                case MessageBoxResult.Yes:
+                    DoctorStartWindow doctorStartWindow = new DoctorStartWindow();
+                    doctorStartWindow.Show();
+                    this.Close();
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+        }
+
+        private void ExaminationsButtonClick(object sender, RoutedEventArgs e)
+        {
+            DoctorStartWindow doctorStartWindow = new DoctorStartWindow();
+            doctorStartWindow.Show();
+            this.Close();
+        }
+
+        private void OperationButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void NotificationsButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StatisticsButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SingOutButtonClick(object sender, RoutedEventArgs e)
+        {
+            userService.LogOut();
+            this.Close();
+        }
+
+        private void SettingsButtonClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
