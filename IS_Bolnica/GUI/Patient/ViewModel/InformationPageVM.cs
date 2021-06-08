88408abcdaa072cae1145559a3c84bbdc4b82aa2
vrba3 <vrbica.vlado11@gmail.com>
2021-06-09 00:51:14 +0200

    Revert "Revert "dodata jos jedna klasa u mvvm""
    This reverts commit 3ac50668ccf910c971f0be783108adae1ac58459.

---
+++ IS_Bolnica/GUI/Patient/ViewModel/InformationPageVM.cs
@@ -0,0 +1,55 @@
+﻿using IS_Bolnica.GUI.Patient.Command;
+using IS_Bolnica.PatientPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS_Bolnica.GUI.Patient.ViewModel
+{
+    class InformationPageVM
+    {
+        #region Properties
+
+        public string TitleText { get; set; }
+        public string InformationText { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public InformationPageVM(String title, String text)
+        {
+            SetCommands();
+            TitleText = title;
+            InformationText = text;
+        }
+
+        #endregion
+
+        #region Commands
+
+        public RelayCommand BackButtonCommand { get; private set; }
+
+        #endregion
+
+        #region CommandActions
+
+        private void BackExecute(object parameter)
+        {
+            PatientWindow.MyFrame.NavigationService.Navigate(new MyAppointments());
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void SetCommands()
+        {
+            BackButtonCommand = new RelayCommand(BackExecute);
+        }
+
+        #endregion
+    }
+}
