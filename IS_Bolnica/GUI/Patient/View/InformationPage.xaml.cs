7ccc7e506ea52b73d6cc5b8a6fb36162483335e4
vrba3 <vrbica.vlado11@gmail.com>
2021-06-09 00:36:53 +0200

    dodata jos jedna klasa u mvvm

---
+++ IS_Bolnica/GUI/Patient/View/InformationPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using IS_Bolnica.GUI.Patient.ViewModel;
+using IS_Bolnica.PatientPages;
+
+namespace IS_Bolnica.GUI.Patient.View
+{
+    /// <summary>
+    /// Interaction logic for InformationPage.xaml
+    /// </summary>
+    public partial class InformationPage : Page
+    {
+        public InformationPage(String title, String text)
+        {
+            InitializeComponent();
+
+            DataContext = new InformationPageVM(title, text);
+        }
+    }
+}
